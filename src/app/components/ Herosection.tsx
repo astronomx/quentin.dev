de2f2b14705f633c7444d2d811f65a1b53b04412
Quentin Valen <quentinvalen@gmail.com>
2026-05-06 16:34:18 +0200

    Compact & Expanded stat card
    Extra option for the stat card for when circle icon is selected, this way you could a internship for example

--- src/app/components/ Herosection.tsx
+++ src/app/components/ Herosection.tsx
@@ -2,7 +2,7 @@ import StatCard from "./StatCard";
 
 export default function Herosection() {
     return(
-        <div className="flex flex-col md:flex-row w-full min-h-dvh bg-warm-amber">
+        <div className="flex flex-col md:flex-row w-full">
             <div className="flex flex-col gap-5 w-full lg:w-1/2 xl:p-48">
                 <h2 className="text-5xl sm:text-6xl">I build fast, clean <span className="text-(--violet) font-mono">digital things.</span></h2>
                 <p className="opacity-80 text-md/4">
@@ -11,10 +11,18 @@ export default function Herosection() {
                 </p>
             </div>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-              <StatCard title="Projects" titleTop={true} content="6" items={[]} />
-              <StatCard title="Interships done" titleTop={false} content="1" items={[]} />
-              <StatCard title="Techstack" titleTop={true} content="" items={["NextJs", "Typescript", "Tailwind"]} />
+            <div className="flex flex-col gap-3">
+                <div className="flex flex-col md:flex-row w-full gap-3">
+                    <StatCard content="3" contentInfo="Projects shipped" icon="none" />
+
+                    <StatCard content="1" contentInfo="Interships done" icon="X" />
+                </div>
+
+                <StatCard title="Internship" content="What did u do?" contentInfo="US media" role="Intern/Junior dev"
+                    icon="Circle" period={{ startDate: "Jun 2025", endDate: "Sep 2025" }} />
+
+                <StatCard title="Techstack" icon="none"
+                    items={["NextJs", "Typescript", "Tailwind", "VueJs", "Java", "Figma", "Git"]} />
             </div>
         </div>
     )
--- src/app/components/StatCard.tsx
+++ src/app/components/StatCard.tsx
@@ -1,24 +1,66 @@
 type StatCardType = {
-    title: string,
-    titleTop: boolean,
-    content: string,
-    items: string[]
+    title?: string | "none",
+    content?: string,
+    contentInfo?: string | "none",
+    role?: string | "none",
+    period?: PeriodType,
+    icon: "Plus" | "X" | "Circle" | "none",
+    items?: string[]
 }
 
-export default function StatCard({title, titleTop, content, items}: StatCardType) {
+type PeriodType = {
+    startDate: string,
+    endDate: string
+}
+
+export default function StatCard({title, content, contentInfo, role, period, icon, items}: StatCardType) {
+    const isCompactCard = icon === "Plus" || icon === "X" || icon === "none"
+
     return(
-        <div className={`flex ${titleTop ? "flex-col" : "flex-col-reverse"} 
-            bg-zinc-300 dark:bg-[#111827]`}>
-                
-            <p>{title}</p>
-            <p>{content}</p>
+        <div className={`flex flex-col bg-white p-3 border border-gray-200 rounded-xl h-24 w-full
+            justify-center`}>
+
+            {isCompactCard ? (
+                <div className="">
+                    <p className="text-xs opacity-40 font-mono uppercase">{title}</p>
+
+                    <div className="flex flex-col">
+                        <p className="text-xl">{content}</p>
+                        <p className="text-sm">{contentInfo}</p>
+                    </div>
+                </div>
+            ) : (
+                <div className="flex justify-between">                    
+                    <div className="flex flex-col">
+                        <div className="flex flex-row justify-between">
+                            <p className="text-xs opacity-40 font-mono uppercase">{title}</p>
+
+                            <div className="flex flex-row bg-background rounded-full px-2 py-0.5 text-xs opacity-50 gap-1">
+                                <p>{period?.startDate}</p>
+                                -
+                                <p>{period?.endDate}</p>
+                            </div>
+                        </div>
+
+                        <div className="flex flex-col gap-1">
+                            <p className="text-lg font-bold">{contentInfo}</p>
+
+                            <div className="flex flex-row gap-3">
+                                <p className="text-xs">{role}</p>
+                                {icon}
+                                <p className="text-xs">{content}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
             
             <div className="flex flex-row gap-1">
-                {items.map((item) => (
+                {items?.map((item) => (
                     <span
                         key={item}
                         className="rounded-md py-0.5 px-1 font-mono
-                            text-xs w-fit"
+                            bg-background text-xs w-fit"
                     >
                         {item}
                     </span>
