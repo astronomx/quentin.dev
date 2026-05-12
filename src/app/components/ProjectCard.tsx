edd9af31ae71e7092d9fbc68a62b9a8647646147
Quentin Valen <quentinvalen@gmail.com>
2026-05-12 16:16:33 +0200

    New layout
    Put the title in the picutre which later might change to go over the bottom the half of the picture. The preview text and go to button are placed under the picture

--- src/app/components/ProjectCard.tsx
+++ src/app/components/ProjectCard.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import { ArrowLongRightIcon } from "@heroicons/react/24/outline";
+
 type ProjectCardType = {
     title: string,
     previewText: string,
@@ -10,24 +12,35 @@ type ProjectCardType = {
 export default function ProjectCard({title, previewText, image}: ProjectCardType) {
     return (
         <div className="h-56 w-80 rounded-lg flex flex-col">
-            <h2 className="font-bold text-2xl text-(--slate-blue)">{ title }</h2>
+            <div className="z-20 bg-background w-fit h-fit py-0.5 px-2 rounded-tr-lg">
+                <h2 className="font-bold text-xl text-(--slate-blue)">{ title }</h2>
+            </div>
 
 
-            <div className="relative">
+            <div className="relative flex flex-col-reverse">
                 <Image src={image} alt={`${title}`} height={224} width={320}
                     className="absolute flex-1 rounded-lg border-background-muted group
                         " 
                     />
+            </div>
 
-                <div className="absolute z-10 flex flex-col text-xs gap-3 p-3 bg-white w-full h-fit">
-                    <p className="">{ previewText }</p>
+            <div className="flex flex-col text-xs gap-3 w-full h-fit mt-3">
+                <p className="">{ previewText }</p>
 
-                    <Link href={"/"} className="bg-background text-xs py-0.5 px-1 h-fit w-fit
-                        rounded-md drop-shadow-sm cursor-pointer"
+                <div className="flex flex-row gap-1 items-center group transition-all">
+                    <Link
+                        href={"/"}
+                        className="text-xs underline underline-offset-4
+                            rounded-lg drop-shadow-sm cursor-pointer transition-all duration-200"
                     >
                         Go to project 
                     </Link>
+
+                    <ArrowLongRightIcon
+                        className="h-5 w-5 text-foreground transition-transform duration-200 group-hover:translate-x-1"
+                    />
                 </div>
+          
             </div>
         </div>
     )
